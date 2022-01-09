cfbe84a34b76b625875895f023445fd6d7aaa78b
dolgogae <ohsh0731@gmail.com>
2022-01-09 21:30:07 +0900

    delete

--- AI/1_math/ch01/3-4-3_Softmax_in_Dense_Layers.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Dense
-
-logit = tf.random.uniform(shape=(8,5), minval=-10, maxval=10)
-dense = Dense(units=8, activation='softmax')
-
-Y = dense(logit)
-print(tf.reduce_sum(Y, axis=1))
--- AI/1_math/ch01/6-4-2_Zero_Padding_with_Con2D_Layers.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-from tensorflow.keras.layers import Conv2D
-
-images = tf.random.normal(shape=(1,28,28,3))
-conv = Conv2D(filters=1, kernel_size=3, padding='same')
-y = conv(images)
-
-print(y.shape)

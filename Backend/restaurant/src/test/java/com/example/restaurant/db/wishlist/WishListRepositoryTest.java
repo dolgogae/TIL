c535da559604bfff7d808054e822f5b3eb1e25c7
dolgogae <ohsh0731@gmail.com>
2022-02-07 23:35:56 +0900

    220207

---
+++ Backend/restaurant/src/test/java/com/example/restaurant/db/wishlist/WishListRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.example.restaurant.db.wishlist;
+
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import com.example.restaurant.wishlist.repository.WishListRepository;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class WishListRepositoryTest {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    public WishListEntity create(){
+        var wishList = new WishListEntity();
+        wishList.setTitle("title");
+        wishList.setCategory("category");
+        wishList.setAddress("address");
+        wishList.setRoadAddress("roadAddress");
+        wishList.setHomePageLink("");
+        wishList.setImageLink("");
+        wishList.setVisit(false);
+        wishList.setVisitCount(0);
+        wishList.setLastVisitDate(null);
+
+        return wishList;
+    }
+
+    @Test
+    public void saveTest(){
+        var wishList = create();
+
+        var expected = wishListRepository.save(wishList);
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1, expected.getIndex());
+    }
+
+    @Test
+    public void findByIdTest() {
+        var wishList = create();
+        wishListRepository.save(wishList);
+
+        var expected = wishListRepository.findById(1);
+
+        Assertions.assertEquals(true, expected.isPresent());
+        Assertions.assertEquals(1, expected.get().getIndex());
+    }
+
+    @Test
+    public void deleteTest(){
+        var wishList = create();
+        wishListRepository.save(wishList);
+        wishListRepository.deleteById(1);
+
+        int count = wishListRepository.listAll().size();
+
+        Assertions.assertEquals(0, count);
+    }
+
+    @Test
+    public void listAllTest(){
+        var wishList1 = create();
+        wishListRepository.save(wishList1);
+        var wishList2 = create();
+        wishListRepository.save(wishList2);
+        
+        int count = wishListRepository.listAll().size();
+
+        Assertions.assertEquals(2, count);
+    }
+}

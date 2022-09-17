42183a4b9fe6f65eebe893113bac022d3fdf7c08
dolgogae <ohsh0731@gmail.com>
2022-09-17 12:06:24 +0900

    jpql 기본 코드

--- Backend/JPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Backend/JPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -54,7 +56,30 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch){
-        return null;
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if(StringUtils.hasText(memberName)){
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus orderStatus){
+        if(orderStatus == null){
+            return null;
+        }
+        return QOrder.order.status.eq(orderStatus);
     }
 
     /**

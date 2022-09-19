ed5078aff16c48d4220c7b1a435f46a3f53d8e9f
dolgogae <ohsh0731@gmail.com>
2022-09-19 21:30:43 +0900

    영속성 컨텍스트

--- Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,103 +8,61 @@ import javax.persistence.Persistence;
 import org.hibernate.Hibernate;
 
 public class JpaMain {
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-
-//             /* ex2 */
-//             Team team = new Team();
-//             team.setName("abdc");
-//             em.persist(team);
-
-//             Member member = new Member();
-//             member.setUsername("kim");
-//             member.setTeam(team);
-//             em.persist(member);
-
-//             team.addMember(member);
-
-//             // 역방향에선 값을 넣어줘도 DB에 반영이 되지 않는다.
-
-// //            em.flush();
-// //            em.clear();
-
-//             Member findMember = em.find(Member.class, member.getId()); // 1차 캐시(DB에 select query가 안나갔다.)
-//             List<Member> members = findMember.getTeam().getMembers();
-//             for(Member m : members){
-//                 System.out.println("member name = "+m.getUsername());
-//             }
-// //            /* ex1 */
-// //            Member member = new Member();
-// //            member.setId(2L);
-// //            member.setUsername("park");
-// //
-// //            // id generator를 할 경우 영속성 컨텍스트에서는 persist 단계에서 DB에 집어 넣는다.
-// //            // 단, sequence옵션일 경우에는 트랜잭션 커밋 시점에 업데이트 된다.
-// //            em.persist(member);
-// //
-// //            Member findMember = em.find(Member.class, 1L);
-// //            System.out.println("findMember id: "+ findMember.getId());
-// //            System.out.println("findMember name: "+ findMember.getUsername());
-// //
-// //            List<Member> members = em.createQuery("select m from Member as m", Member.class)
-// //                    .setFirstResult(5)
-// //                    .setMaxResults(8)
-// //                    .getResultList();
-// //
-// //            // 변경감지가 된다.
-// //            // 따로 persist를 호출하지 않아도 자동으로 변경된다.(dirty checking)
-// //            findMember.setUsername("drogba");
-// //
-// //            em.remove(findMember);
-// //
-// //            // 강제로 DB에 쿼리를 반영한다.
-// //            em.flush();
-// //
-// //            // transaction commit 시점에서 SQL 쿼리가 나간다.
-// //            // buffering 같은 개념
-//            tx.commit();
-
-            /** 
-             * 프록시 
-             */
+            // 비영속 상태
+            // entity 객체를 생성하고 준비해둔 상태
             Member member = new Member();
-            member.setUsername("hello");
+            member.setId(100L);
+            member.setUsername("HelloJPA");
 
+            // 영속
+            // 영속성 컨텍스트에 의해 관리가 되는 상태
+            // 아직까지 DB에 쿼리가 날아간 상태는 아니다.
+            // 트랜잭션을 commit하는 순간 쿼리가 날아간다. -> 그 전에는 쓰기 지연 SQL 저장소에 저장해둔다.
+            // batch로 한꺼번에 쿼리를 보내서 속도의 이점을 볼 수 있다.
             em.persist(member);
-            
-            em.flush();
-            em.clear();
 
-            // Member findMember = em.find(Member.class, member.getId());
-            Member findMember1 = em.getReference(Member.class, member.getId());
-            System.out.println("findMember: "+findMember1.getUsername());
+            // 영속성 컨텍스트에서 관리하지 않는 상태로 만든다.
+            em.detach(member);
+            // DB에서 지우는 요청
+            em.remove(member);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
+            /**
+             * 영속성 컨텍스트는 1차 캐시를 가지고 있다.
+             * persist를 하게 되면, DB로 바로 저장하는 것이 아닌
+             * 1차 캐시에 저장을 하고, 조회 또한 1차 캐시에서 조회하게 된다.
+             * 만약 조회시 1차 캐시에 없다면, DB를 조회하고 조회한 내용을 1차캐시에 저장을 하고 반환하게 된다.
+             *
+             * entity manager는 트랜잭션 단위에서만 존재하고 소멸하기 때문에 큰 효과를 볼 수 없다.
+             */
+            // select 쿼리가 날아가지 않는다. -> 1차 캐시에 저장된것 가져오기 때문에
+            Member a = em.find(Member.class, 100L);
+            Member b = em.find(Member.class, 100L);
 
-            Member findMember2 = em.getReference(Member.class, member.getId());
-            
-            System.out.println((findMember1.getClass() == findMember2.getClass()));
-            System.out.println((findMember1 instanceof Member));
-            
-            Member reference = em.getReference(Member.class, member.getId());
 
-            // 항상 true로 나와야 한다.
-            System.out.println(findMember1 == reference);
+            // true가 나온다.
+            // 영속 엔티티의 동일성을 보장해준다. 1차 캐시를 했기 때문에
+            System.out.println("result: " + (a == b));
 
-            Member refMember = em.getReference(Member.class, member.getId());
-            Hibernate.initialize(refMember);    // 강제 초기화
-            Member findMember = em.find(Member.class, member.getId());
-
-            System.out.println(refMember == findMember);
+            /**
+             * 변경감지
+             * 따로 update 쿼리 명령을 소스코드로 짜놓지 않아도
+             * 변경감지를 통해서 알아서 DB에 변경된다.
+             *
+             * DB 커밋 시점에서 flush()를 호출하게 된다.
+             * 이후, entity와 스냅샷을 비교한다.(스냅샷: 최초 상태)
+             * 변경되어있는 것이 있다면 쓰기 지연 SQL저장소에 관련 쿼리를 저장해놓게 된다.
+             */
+            b.setUsername("JPAHello");
 
             tx.commit();
+
         } catch (Exception e){
             e.printStackTrace();
             tx.rollback();
@@ -112,7 +70,6 @@ public class JpaMain {
             em.close();
         }
 
-//        em.close();
         emf.close();
     }
 

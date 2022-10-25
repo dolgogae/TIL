a8856825f01d0c7c859b171c1a4de191d3691acd
dolgogae <ohsh0731@gmail.com>
2022-10-25 10:27:49 +0900

    new project

--- Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import org.hibernate.Hibernate;
-
-
 import java.time.LocalDateTime;
 import java.util.List;
 
